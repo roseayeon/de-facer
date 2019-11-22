5ff642322983717aec3164d3ee432a8d729623af
JaeEui Ma <mjy0503@gmail.com>
2019-11-22 09:31:43 +0000

    processing with multiple targets

--- server/face.py
+++ server/face.py
@@ -14,7 +14,7 @@ def prewhiten(x):
   return y
 
 FACE_SIZE = 160
-DIFF_THRESHOLD = 0.8
+DIFF_THRESHOLD = 0.9
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 mtcnn = MTCNN(keep_all=True, min_face_size=20, device=device)
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
@@ -22,13 +22,16 @@ resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 def process_video(video_path, targets_path, replace_path, output_path):
   cap = cv2.VideoCapture(video_path)
   count = 0
-  targets_encoding = []
+  # encoding target face
+  targets_img = []
   for target_path in targets_path:
     target_img = cv2.imread(target_path, cv2.IMREAD_COLOR)
     target_img = Image.fromarray(cv2.cvtColor(target_img, cv2.COLOR_BGR2RGB))
-    target_aligned = mtcnn(target_img)
-    target_encoding = resnet(target_aligned.to(device)).detach().cpu()
-    targets_encoding.append(target_encoding)
+    targets_img.append(target_img)
+
+  targets_aligned = mtcnn(targets_img)
+  targets_aligned = [target[0] for target in targets_aligned]
+  targets_encoding = resnet(torch.stack(targets_aligned).to(device)).detach().cpu()
   
   frames_tracked = []
   while cap:
@@ -37,7 +40,7 @@ def process_video(video_path, targets_path, replace_path, output_path):
       # change fps
       count += 1
       if count % 2 == 0:
-          continue
+        continue
   
       small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
       small_frame = Image.fromarray(cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB))
@@ -49,31 +52,38 @@ def process_video(video_path, targets_path, replace_path, output_path):
       # Draw box of face
       faces = []
       for box in boxes:
-          box = [
-              int(max(box[0]*4, 0)),
-              int(max(box[1]*4, 0)),
-              int(min(box[2]*4, frame.size[0])),
-              int(min(box[3]*4, frame.size[1])),
-          ]
+        box = [
+          int(max(box[0]*4, 0)),
+          int(max(box[1]*4, 0)),
+          int(min(box[2]*4, frame.size[0])),
+          int(min(box[3]*4, frame.size[1])),
+        ]
+
+        face = frame.crop(box).resize((FACE_SIZE, FACE_SIZE), 2)
+        face = prewhiten(F.to_tensor(np.float32(face)).to(device))
+        faces.append(face)
   
-          face = frame.crop(box).resize((face_size, face_size), 2)
-          face = prewhiten(F.to_tensor(np.float32(face)).to(device))
-          faces.append(face)
-  
-      embedding = resnet(torch.stack(faces)).detach().cpu()
+      encodings = resnet(torch.stack(faces)).detach().cpu()
       frame_draw = frame.copy()
       draw = ImageDraw.Draw(frame_draw)
-      for embed, box in zip(embedding, boxes):
-          box = [
-              int(max(box[0]*4, 0)),
-              int(max(box[1]*4, 0)),
-              int(min(box[2]*4, frame.size[0])),
-              int(min(box[3]*4, frame.size[1])),
-          ]
-          face_diff = (embed - target_embedding[0]).norm().item()
+      for encoding, box in zip(encodings, boxes):
+        box = [
+          int(max(box[0]*4, 0)),
+          int(max(box[1]*4, 0)),
+          int(min(box[2]*4, frame.size[0])),
+          int(min(box[3]*4, frame.size[1])),
+        ]
+
+        target_detected = False
+        for target_encoding in targets_encoding: 
+          face_diff = (encoding - target_encoding).norm().item()
           #print (count, face_diff)
-          if face_diff > diff_threshold:
-              draw.rectangle(box, outline=(255, 0, 0), width=6)
+          if face_diff <= DIFF_THRESHOLD:
+            target_detected = True
+            break
+
+        if not target_detected:
+          draw.rectangle(box, outline=(255, 0, 0), width=6)
       
       # Add to frame list
       # doubled frame ti restore fps
@@ -81,6 +91,8 @@ def process_video(video_path, targets_path, replace_path, output_path):
       frames_tracked.append(frame_draw.resize((640, 360), Image.BILINEAR))
       #if count%100 == 0:
       #    print ("frame:", count)
+      if count > 300:
+        break
     else:
       raise Exception('Reading video should be success')
 
